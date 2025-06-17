024f1c153f8f22d5094da69dcd0e09aadcefe1c7
Daniel Tadesse <dtadesse@Daniels-MacBook-Pro-2.local>
2025-06-16 20:19:40 -0400

    home styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => (
-        <section id="home" className="home-section">
-            <div className="home-content">
-                <h1>Welcome to Solomon Ejigu Advisory</h1>
-                <p>Your trusted partner in accounting, tax, financial, and business advisory services.</p>
-            </div>
-        </section>
+    <section className="circle-section">
+        <div className="circle" id="circle-small"></div>
+        <div className="circle" id="circle-medium"></div>
+        <div className="circle" id="circle-large"></div>
+
+        <div className="circle-content">
+            <h1>Welcome to Solomon Ejigu Advisory</h1>
+            <p>Your trusted partner in accounting, tax, financial, and business advisory services.</p>
+        </div>
+    </section>
 );
 
 export default Home;
